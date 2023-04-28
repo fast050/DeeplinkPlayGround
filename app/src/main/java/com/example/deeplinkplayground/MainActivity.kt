b39191ef0801c81324ce18ae43d4e3d0487fc8c6
Khalid <khalidwork050@outlook.com>
2023-04-28 12:57:30 +0400

    init

---
+++ app/src/main/java/com/example/deeplinkplayground/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.deeplinkplayground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.net.Uri
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
